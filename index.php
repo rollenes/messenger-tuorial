9a956575f2bf0afce9345878b022ed8550e211f2
rollen <rafal.wartalski@gmail.com>
2015-02-06 00:57:50 +0100

    Symfony http fundation component introduced

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
-if (@$_POST['recipient'] && @$_POST['message']) {
-    echo '<span id="confirmation">Your message has been sent to: ' . $_POST['recipient'] . '</span>';
+
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+
+$recipient = $request->request->get('recipient');
+$message = $request->request->get('message');
+
+if ($recipient && $message) {
+    echo '<span id="confirmation">Your message has been sent to: ' . $recipient . '</span>';
 }
 ?>
 
