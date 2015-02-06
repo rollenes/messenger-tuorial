9c4e516730a9b593d19349b7f4f57502eda6de91
rollen <rafal.wartalski@gmail.com>
2015-02-06 04:08:44 +0100

    Message specification

--- spec/Domain/MessageSpec.php
+++ spec/Domain/MessageSpec.php
@@ -2,13 +2,34 @@
 
 namespace spec\Domain;
 
+use Domain\Message;
+use Domain\Message\Recipient;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * @mixin Message
+ */
 class MessageSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    private $recipient;
+    private $text;
+
+    public function let()
     {
-        $this->shouldHaveType('Domain\Message');
+        $this->recipient = new Recipient('test@test.pl');
+        $this->text = new Message\Text('test text');
+
+        $this->beConstructedWith($this->recipient, $this->text);
+    }
+
+    function it_has_recipient()
+    {
+        $this->getRecipient()->shouldBe($this->recipient);
+    }
+
+    function it_has_text()
+    {
+        $this->getText()->shouldBe($this->text);
     }
 }
--- src/Domain/Message.php
+++ src/Domain/Message.php
@@ -2,6 +2,34 @@
 
 namespace Domain;
 
+use Domain\Message\Recipient;
+use Domain\Message\Text;
+
 class Message
 {
+    /**
+     * @var Recipient
+     */
+    private $recipient;
+
+    /**
+     * @var Text
+     */
+    private $text;
+
+    public function __construct(Recipient $recipient, Text $text)
+    {
+        $this->recipient = $recipient;
+        $this->text = $text;
+    }
+
+    public function getRecipient()
+    {
+        return $this->recipient;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
 }
