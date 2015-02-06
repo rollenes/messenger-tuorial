c119c99d5394e8952103fdccf18110012a6632ce
rollen <rafal.wartalski@gmail.com>
2015-02-06 03:54:16 +0100

    Some specs for Text

--- spec/Domain/Message/TextSpec.php
+++ spec/Domain/Message/TextSpec.php
@@ -2,13 +2,26 @@
 
 namespace spec\Domain\Message;
 
+use Domain\Message\Text;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * @mixin Text
+ */
 class TextSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_can_be_casted_to_string()
     {
-        $this->shouldHaveType('Domain\Message\Text');
+        $testMessage = 'this is test message';
+        $this->beConstructedWith($testMessage);
+
+        $this->__toString()->shouldBe($testMessage);
+    }
+
+    function it_rises_exception_when_created_with_no_string()
+    {
+        $this->shouldThrow()->during('__construct', [null]);
+        $this->shouldThrow()->during('__construct', [132]);
     }
 }
--- src/Domain/Message/Text.php
+++ src/Domain/Message/Text.php
@@ -4,4 +4,19 @@ namespace Domain\Message;
 
 class Text
 {
+    private $text;
+
+    public function __construct($text)
+    {
+        if (!is_string($text)) {
+            throw new \LogicException('Recipient should be a string');
+        }
+
+        $this->text = $text;
+    }
+
+    public function __toString()
+    {
+        return $this->text;
+    }
 }
