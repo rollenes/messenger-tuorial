811cf4c5937003d446a3a30da5d7866b17adb73f
rollen <rafal.wartalski@gmail.com>
2015-02-06 23:09:31 +0100

    Controller specification

--- spec/App/SendEmailSpec.php
+++ spec/App/SendEmailSpec.php
@@ -2,31 +2,52 @@
 
 namespace spec\App;
 
+use App\SendEmail;
+use App\Sender\Email;
+use Domain\Message;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @mixin SendEmail
+ */
 class SendEmailSpec extends ObjectBehavior
 {
-    function let(\Twig_Environment $twig)
+    function let(\Twig_Environment $twig, Email $emailSender)
     {
-        $this->beConstructedWith($twig);
+        $this->beConstructedWith($twig, $emailSender);
     }
 
-    function it_returns_response()
+    function it_returns_response(Email $emailSender)
     {
-        $request = $this->createRequest();
+        $request = new Request();
+
+        $emailSender->send(Argument::type(Message::class))->shouldNotBeCalled();
 
         $this($request)->shouldBeAnInstanceOf(Response::class);
     }
 
-    private function createRequest()
+    function it_sends_email(Email $emailSender)
     {
-        return new Request([], [
+        $request = $this->createPostRequest();
+
+        $this($request);
+
+        $emailSender->send(Argument::type(Message::class))->shouldHaveBeenCalled();
+    }
+
+    private function createPostRequest()
+    {
+        $request = new Request([], [
             'recipient' => 'test@test.pl',
             'message' => 'Test message'
         ]);
+
+        $request->setMethod('POST');
+
+        return $request;
     }
 
 }
--- src/App/SendEmail.php
+++ src/App/SendEmail.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Sender\Email;
+use Domain\Message;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,13 +14,25 @@ class SendEmail
      */
     private $twig;
 
-    public function __construct(\Twig_Environment $twig)
+    /**
+     * @var Email
+     */
+    private $emailSender;
+
+    public function __construct(\Twig_Environment $twig, Email $emailSender)
     {
         $this->twig = $twig;
+        $this->emailSender = $emailSender;
     }
 
     public function __invoke(Request $request)
     {
+
+        if ($request->isMethod('POST')) {
+            $message = new Message(new Message\Recipient($request->request->get('recipient')), new Message\Text($request->request->get('message')));
+            $this->emailSender->send($message);
+        }
+
         $content = $this->twig->render('send-email.html.twig', [
             'recipient' => $request->request->get('recipient'),
             'message' => $request->request->get('message')
